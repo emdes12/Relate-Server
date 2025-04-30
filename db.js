9971b683aa3bd6b23ca9696383f5cd5a661449b4
Alawiye Muritala <naijabayz@gmail.com>
2025-04-30 16:51:04 +0100

    switch db mode to prod

--- db.js
+++ db.js
@@ -8,19 +8,19 @@ import pg from "pg";
 
 // Number 1
 
-// const db = new pg.Client({
-// connectionString: process.env.DATABASE_URL, // Railway injects this
-// ssl: { rejectUnauthorized: false }, // Required for Railway PostgreSQL
-// });
+const db = new pg.Client({
+connectionString: process.env.DATABASE_URL, // Railway injects this
+ssl: { rejectUnauthorized: false }, // Required for Railway PostgreSQL
+});
 
 // Nimber 2
 
-const db = new pg.Client({
-  host: "localhost",
-  database: "relateplus",
-  user: "postgres",
-  port: 5432,
-  password: "EmDes12@",
-});
+// const db = new pg.Client({
+//   host: "localhost",
+//   database: "relateplus",
+//   user: "postgres",
+//   port: 5432,
+//   password: "EmDes12@",
+// });
 
 export default db; // Changed from module.exports to export default
